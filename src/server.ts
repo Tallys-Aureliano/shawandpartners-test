5bf93de26f3059dbecea61074a346f202982529a
Tallys-aureliano <tallysaureliano@outlook.com>
2023-07-10 20:33:20 -0300

    :sparkles:feat: Adding router

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import { router } from "./routes/index";
 
-const app = express();
-
+const app = express();  
 app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running in port 3333!"));
