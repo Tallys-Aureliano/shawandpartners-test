452246243daee1f38100933bc794e2d8ff40e6af
Tallys-aureliano <tallysaureliano@outlook.com>
2023-07-11 16:23:08 -0300

    :sparkles:feat: New status codes

--- src/app/controllers/uploadUserController.ts
+++ src/app/controllers/uploadUserController.ts
@@ -3,18 +3,16 @@ import { uploadUsersUseCase } from '../usercase/createUsers';
 import { Readable } from 'stream';
 import readline from 'readline';
 import { userDto } from '../dtos/usersDtos';
+import { StatusCodes } from "http-status-codes";
 
 export class UploadUserController {
     async handle(req: Request, res: Response) {
         try {
             const { file } = req;
-
             if (!file) {
-                return res.status(400).json({ error: 'No file provided' });
+                return res.status(StatusCodes.BAD_REQUEST).json({ error: 'No file provided' });
             }
-
             const buffer = file.buffer;
-
             const readableFile = new Readable();
             readableFile.push(buffer);
             readableFile.push(null);
@@ -27,25 +25,21 @@ export class UploadUserController {
 
             for await (const line of usersLine) {
                 const usersLineSplit = line.split(';');
-
                 const [name, city, country, favorite_sport] = usersLineSplit;
-
                 const user: userDto = {
                     name,
                     city,
                     country,
                     favorite_sport,
                 };
-
                 await uploadUsersUseCase(user);
-
                 usersArray.push(user);
             }
+            return res.status(StatusCodes.CREATED).json(usersArray);
 
-            return res.json(usersArray);
         } catch (error) {
             console.error('Error creating users:', error);
-            return res.status(500).json({ error: 'Internal server error' });
+            return res.status(StatusCodes.NOT_FOUND).json({ error: "The server cannot find the requested resource" });
         }
     }
 }
