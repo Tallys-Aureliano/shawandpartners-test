19a60021de4dff5e19442fba53436a5aeffa4dd2
Tallys-aureliano <tallysaureliano@outlook.com>
2023-07-11 12:01:32 -0300

    :recycle:refactor: Routes of aplication
     Now the index file only manages routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,103 +1,12 @@
-import { PrismaClient } from '@prisma/client';
-import { Request, Response, Router } from 'express';
-import multer from 'multer';
-import readline from 'readline';
-import { Readable } from 'stream';
-
-const prisma = new PrismaClient();
-const multerConfig = multer();
+import { Request, Response, Router } from "express";
+import { upload } from "./uploadUser.routes";
 const router = Router();
 
-interface User {
-  name: string;
-  city: string;
-  country: string;
-  favorite_sport: string;
-}
 
-router.post('/users', multerConfig.single('file'), async (request: Request, response: Response) => {
-  try {
-    const { file } = request;
-
-    if (!file) {
-      return response.status(400).json({ error: 'No file provided' });
-    }
-
-    const buffer = file.buffer;
-
-    const readableFile = new Readable();
-    readableFile.push(buffer);
-    readableFile.push(null);
-
-    const usersLine = readline.createInterface({
-      input: readableFile,
-    });
-
-    const usersArray: User[] = [];
-    
-    for await (const line of usersLine) {
-      const usersLineSplit = line.split(';');
-
-      const [name, city, country, favorite_sport] = usersLineSplit;
-
-      const user: User = {
-        name,
-        city,
-        country,
-        favorite_sport,
-      };
-
-      await prisma.user.create({
-        data:{
-            name,
-            city,
-            country,
-            favorite_sport,
-        }
-      });
-
-      usersArray.push(user);
-    }
-
-    return response.json(usersArray);
-  } catch (error) {
-    console.error('Error creating users:', error);
-    return response.status(500).json({ error: 'Internal server error' });
-  }
+router.get("/",(req:Request, res:Response) => {
+    return res.status(200).send({success:"Server ON and Connect!"});
 });
 
-router.get('/get:value?', async (req: Request, res: Response) => {
-  const { value } = req.params;
-    if (!value) {
-    const listUser = await prisma.user.findMany({
-      select: {
-        name: true,
-        city: true,
-        country: true,
-        favorite_sport: true
-      },
-    }); 
-    return res.status(210).json(listUser); 
-  } else {
-    const search = await prisma.user.findMany({
-      where: {
-        OR: [
-          { name: value},
-          { city: value },
-          { country: value },
-          { favorite_sport: value },
-        ],
-      },
-      select: {
-        name: true,
-        city: true,
-        country: true,
-        favorite_sport: true,
-      },
-    });
-    console.log(search);
-    return res.json(search);
-  }
-});
+router.use("/users", upload);
 
-export { router };
+export {router};
