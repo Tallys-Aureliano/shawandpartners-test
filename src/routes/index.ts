24b7daa5307c95740399468fe918a4f30f998a73
Tallys-aureliano <tallysaureliano@outlook.com>
2023-07-10 23:08:27 -0300

    :sparkles:feat: create post

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,52 @@
+import { PrismaClient} from "@prisma/client";
+const prisma = new PrismaClient();
 import { Request, Response, Router} from "express";
 import multer from "multer";
+import readline from "readline";
+import { Readable } from "stream";
 
 const multerConfig = multer();
 const router = Router();
 
-router.post("/users", 
-                multerConfig.single("file"), 
-                    (request: Request, response: Response) => {
-                        console.log(request.file); 
-                            return response.send();
-            });
+interface User{
+    name: string;
+    city: string;
+    country: string;
+    favorite_sport: string;
+}
+
+router.post("/users", multerConfig.single("file"), 
+async (request: Request, response: Response)=> { 
+    const { file } = request;
+    const buffer = file?.buffer;
+    
+    const readableFile = new Readable();
+    readableFile.push(buffer);
+    readableFile.push(null); 
+    const usersLine = readline.createInterface({
+        input: readableFile,
+    });
+    const usersArray: User[] = [];
+    for await (let line of usersLine) {
+        const usersLineSplit = line.split(";");   
+        usersArray.push({
+            name: usersLineSplit[0],
+            city: usersLineSplit[1],
+            country: usersLineSplit[2],
+            favorite_sport: usersLineSplit[3] 
+        });
+    }
+    for await (let {name, city, country, favorite_sport} of usersArray){
+        await prisma.user.create({
+            data: {
+                name,
+                city,
+                country,
+                favorite_sport
+            },
+        });
+    }
+    return response.send();
+});
 
 export {router};
