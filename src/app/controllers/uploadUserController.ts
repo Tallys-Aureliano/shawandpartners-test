a6b0ea85a6c2876dbc00e58d1be684a454e4334e
Tallys-aureliano <tallysaureliano@outlook.com>
2023-07-12 10:09:28 -0300

    recycle:refactor: removing unused codes

--- src/app/controllers/uploadUserController.ts
+++ src/app/controllers/uploadUserController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { uploadUsersUseCase } from '../usercase/uploadUsers';
+import { UploadUsersUseCase } from '../usercase/uploadUsers';
 import { Readable } from 'stream';
 import readline from 'readline';
 import { userDto } from '../dtos/usersDtos';
@@ -10,7 +10,8 @@ export class UploadUserController {
         try {
             const { file } = req;
             if (!file) {
-                return res.status(StatusCodes.BAD_REQUEST).json({ error: 'No file provided' });
+                console.log(file);
+                return res.status(StatusCodes.BAD_REQUEST).send({ error: 'No file provided' });
             }
             const buffer = file.buffer;
             const readableFile = new Readable();
@@ -22,7 +23,7 @@ export class UploadUserController {
             });
 
             const usersArray: userDto[] = [];
-            const create = new uploadUsersUseCase();
+            const create = new UploadUsersUseCase();
             for await (const line of usersLine) {
                 const usersLineSplit = line.split(';');
                 const [name, city, country, favorite_sport] = usersLineSplit;
@@ -32,13 +33,12 @@ export class UploadUserController {
                     country,
                     favorite_sport,
                 };
-                const newUser = await create.execute(user);
+                await create.execute(user);
                 usersArray.push(user);
             }
             return res.status(StatusCodes.CREATED).json(usersArray);
 
         } catch (error) {
-            console.error('Error creating users:', error);
             return res.status(StatusCodes.NOT_FOUND).json({ error: "The server cannot find the requested resource" });
         }
     }
